9eaecb317d5aca26404cc4eec41aa559a2dac8aa
Ainesh Sinha <studentainesh@gmail.com>
2023-12-23 01:43:10 +0530

    Updates for 23rd December

--- src/Chat.js
+++ src/Chat.js
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Chat.css' ;
 import { useParams } from 'react-router-dom';
+import { StarBorderOutlined } from '@mui/icons-material';
+import { InfoOutlined } from '@mui/icons-material';
+import db from './firebase';
 
 function Chat() {
     const {roomId} = useParams();
+    const {roomDetails , setRoomDetails} = useState(null);
+
+    useEffect(() =>{
+        if(roomId){
+            //TODO: Update as per v9.0+ of Firebase
+            db.collection('rooms').document(roomId).onSnapshot(snapshot => (
+                setRoomDetails(snapshot.data())
+            ))
+        }
+    } , [roomId])
+
     return (
         <div className='chat'>
-            <h2>You are in the {roomId} room</h2>
             <div className='chat_header'>
-                
+                <div className='chat_headerLeft'>
+                    <h4 className='chat_channelName'>
+                        <strong># general</strong>
+                        <StarBorderOutlined/>
+                    </h4>
+                </div>
+                <div className='chat_headerRight'>
+                    <p>
+                        <InfoOutlined/> Details
+                    </p>
+                </div>
             </div>
         </div>
   )
--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import './SidebarOption.css'
+import {useNavigate} from 'react-router-dom';
+import db from './firebase';
 
+function SidebarOption({Icon , title , id , addChannelOption}) {
+  const history = useNavigate();
 
-function SidebarOption({Icon , title}) {
+  
+  const selectChannel = () => {
+    if(id){
+      history(`/room/${id}`);
+    }
+    else{
+      history(title);
+    }
+  }
+
+  const addChannel = () => {
+    const channelName = prompt(`Please enter the channel name that you want to add: `);
+
+    if(channelName){
+      db.collection('rooms').add({
+        name: channelName,
+      })
+    }
+  }
+
   return (
-    <div className='sidebarOption'>
+    <div className='sidebarOption' onClick={addChannelOption ? addChannel : selectChannel} >
       {Icon && <Icon className='sidebarOption_icon' />}
       {Icon ? (<h3>{title}</h3>):(
       <h3 className='sidebarOption_channel'>
